7bda0740d637cbacb43745444867ffc089838b3e
Thisarakavinda1 <thisarak84@gmail.com>
2024-07-27 14:15:56 +0530

    small change nav bar
    small change nav bar

--- Doctor/doctordashboard2.php
+++ Doctor/doctordashboard2.php
@@ -72,7 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if ($stmt->execute()) {
-        $_SESSION['message'] = "Profile updated successfully!";
+        $_SESSION['message'] = "";
         header("Location: doctordashboard.php"); // Redirect to the dashboard
         exit();
     } else {
@@ -117,7 +117,7 @@ $con->close();
 <body class="d-flex flex-column min-vh-100">
 <main class="flex-fill">
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg sticky-top" style="background-color: #6295a2;">
+    <nav class="navbar navbar-expand-lg sticky-top py-1" style="background-color: #6295a2;">
         <div class="container">
             <a class="navbar-brand" href="index.php">
                 <img src="../images/logo.png" alt="Logo" width="50" height="40">
